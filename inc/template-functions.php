6354a16bdccff20f4475b37b00d7cf86b27e16e5
Jalil Nouri <nouri.6412@gmail.com>
2022-03-09 11:11:55 +0330

    123

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,34 +1,36 @@
 <?php
+
 /**
  * Filters the default archive titles.
  */
-function custom_theme_get_the_archive_title() {
-	if ( is_category() ) {
-		$title = 'دسته' .' '. '<span class="page-description">' . single_term_title( '', false ) . '</span>';
-	} elseif ( is_tag() ) {
-		$title = 'تگ' .' '. '<span class="page-description">' . single_term_title( '', false ) . '</span>';
-	} elseif ( is_author() ) {
-		$title = 'نویسنده'.' ' . '<span class="page-description">' . get_the_author_meta( 'display_name' ) . '</span>';
-	} elseif ( is_year() ) {
-		$title = 'سال' .' '. '<span class="page-description">' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentynineteen' ) ) . '</span>';
-	} elseif ( is_month() ) {
-		$title = 'ماه'.' ' . '<span class="page-description">' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentynineteen' ) ) . '</span>';
-	} elseif ( is_day() ) {
-		$title = 'روز' .' '. '<span class="page-description">' . get_the_date() . '</span>';
-	} elseif ( is_post_type_archive() ) {
-		$title = '' . '<span class="page-description">' . post_type_archive_title( '', false ) . '</span>';
-	} elseif ( is_tax() ) {
-		$tax = get_taxonomy( get_queried_object()->taxonomy );
-		/* translators: %s: Taxonomy singular name. */
-		$title = sprintf( esc_html__( '%s :', 'twentynineteen' ), $tax->labels->singular_name );
-	} else {
-		$title = '';
-	}
-	return $title;
+function custom_theme_get_the_archive_title()
+{
+    if (is_category()) {
+        $title = 'دسته' . ' ' . '<span class="page-description">' . single_term_title('', false) . '</span>';
+    } elseif (is_tag()) {
+        $title = 'تگ' . ' ' . '<span class="page-description">' . single_term_title('', false) . '</span>';
+    } elseif (is_author()) {
+        $title = 'نویسنده' . ' ' . '<span class="page-description">' . get_the_author_meta('display_name') . '</span>';
+    } elseif (is_year()) {
+        $title = 'سال' . ' ' . '<span class="page-description">' . get_the_date(_x('Y', 'yearly archives date format', 'twentynineteen')) . '</span>';
+    } elseif (is_month()) {
+        $title = 'ماه' . ' ' . '<span class="page-description">' . get_the_date(_x('F Y', 'monthly archives date format', 'twentynineteen')) . '</span>';
+    } elseif (is_day()) {
+        $title = 'روز' . ' ' . '<span class="page-description">' . get_the_date() . '</span>';
+    } elseif (is_post_type_archive()) {
+        $title = '' . '<span class="page-description">' . post_type_archive_title('', false) . '</span>';
+    } elseif (is_tax()) {
+        $tax = get_taxonomy(get_queried_object()->taxonomy);
+        /* translators: %s: Taxonomy singular name. */
+        $title = sprintf(esc_html__('%s :', 'twentynineteen'), $tax->labels->singular_name);
+    } else {
+        $title = '';
+    }
+    return $title;
 }
-add_filter( 'get_the_archive_title', 'custom_theme_get_the_archive_title' );
+add_filter('get_the_archive_title', 'custom_theme_get_the_archive_title');
 
-function custom_generate_menu_li($navItem, $menu)
+function custom_generate_menu_li_mobile($navItem, $menu)
 {
     $item_class = "";
     $i = '';
@@ -36,30 +38,74 @@ function custom_generate_menu_li($navItem, $menu)
     if (trim($actual_link) == $navItem->url && $navItem->menu_item_parent == 0) {
         $item_class = 'active';
     }
+?>
+    <?php
+    if (!isset($menu[$navItem->ID])) {
+    ?>
+        <li class="nav-item">
+            <a href="<?php echo $navItem->url; ?>" class="nav-link <?php echo $item_class ?>"><?php echo $navItem->title; ?></a>
+        </li>
+    <?php
+    } else {
+    ?>
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header nav-item" id="flush-headingOne">
+                    <a href="<?php echo $navItem->url; ?>" class="accordion-button collapsed nav-link" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                        <?php echo $navItem->title; ?>
+                    </a>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body ps-0">
+                        <ul class="p-0 m-0">
+                            <?php
+                            foreach ($menu[$navItem->ID] as $item) {
+                                custom_generate_menu_li_mobile($item, $menu);
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+<?php
+}
+
+function custom_generate_menu_li($navItem, $menu)
+{
+    $item_class = "";
+    $child = '';
+    $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    if (trim($actual_link) == $navItem->url && $navItem->menu_item_parent == 0) {
+        $item_class = 'active';
+    }
     if (isset($menu[$navItem->ID])) {
-        if ($navItem->menu_item_parent == 0) {
-            $i = '<i class="fa fa-chevron-down"></i>';
-        } else {
-            $i = '<i class="fa fa-angle-right"></i>';
-        }
+        $child = 'position-relative child';
     }
 ?>
-    <li class="<?php echo $item_class; ?>">
-        <a href="<?php echo $navItem->url; ?>"><?php echo $navItem->title; ?><?php echo $i; ?></a>
-        <?php
-        if (isset($menu[$navItem->ID])) {
-        ?>
-            <ul class="sub-menu">
+    <?php
+    if (!isset($menu[$navItem->ID])) {
+    ?>
+        <li class="">
+            <a href="<?php echo $navItem->url; ?>" class="<?php echo $item_class ?>"><?php echo $navItem->title; ?></a>
+        </li>
+    <?php
+    } else {
+    ?>
+        <li class="<?php echo $child ?>"><a href="<?php echo $navItem->url; ?>"><?php echo $navItem->title; ?></a>
+            <ul class="p-0 m-0 inner">
                 <?php
                 foreach ($menu[$navItem->ID] as $item) {
                     custom_generate_menu_li($item, $menu);
                 }
                 ?>
-
             </ul>
-        <?php
-        }
-        ?>
-    </li>
+        </li>
+    <?php
+    }
+    ?>
 <?php
 }
--- template-parts/menu/menu-content.php
+++ template-parts/menu/menu-content.php
@@ -1,4 +1,5 @@
 <?php
+$header = get_field("header-iis", "option");
 $menuLocations = get_nav_menu_locations();
 
 $menuID = $menuLocations['primary-menu'];
@@ -8,10 +9,45 @@ $menus = [];
 //var_dump($primaryNav);
 $menus = get_menu_array_nav_item($primaryNav);
 ?>
-<ul class="nav navbar-nav">
-    <?php
-    foreach ($menus[0] as $navItem) {
-        custom_generate_menu_li($navItem, $menus);
-    }
-    ?>
-</ul>
+
+<!-- navigation -->
+
+<!-- mobile-menu -->
+<div class="text-end follow-btn d-md-none">
+    <a href="<?php echo (isset($header["follow_us"])) ? $header["follow_us"] : ''; ?>" class="btn btn-primary"><?php _e('Follow us', 'iisgroup');  ?></a>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light d-md-none ">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 pt-3">
+                <?php
+                foreach ($menus[0] as $navItem) {
+                    custom_generate_menu_li_mobile($navItem, $menus);
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<!-- desktop-manu-->
+<nav class="pt-3 pb-3 ps-xl-5 pe-xl-5 d-none d-md-block">
+    <div class="row align-items-center">
+        <div class="col-9">
+            <ul class="p-0 m-0 d-flex menu-list align-items-center">
+                <?php
+                foreach ($menus[0] as $navItem) {
+                    custom_generate_menu_li($navItem, $menus);
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="col-3 text-end">
+            <a href="<?php echo (isset($header["follow_us"])) ? $header["follow_us"] : ''; ?>" class="btn btn-primary"><?php _e('Follow us', 'iisgroup');  ?></a>
+        </div>
+    </div>
+</nav>
